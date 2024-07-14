9a26de7b04171227ee40d5a72274fa59bceb59ca
BillJPG <v.tarnwuttipong@gmail.com>
2024-07-14 20:32:33 +0900

    Delete businessworldtemplate copy directory

--- businessworldtemplate copy/scripts.js
+++
@@ -1,6 +0,0 @@
-function askName() {
-    let userName = prompt("Please enter your name:");
-    if (userName) {
-        document.getElementById("welcomeMessage").innerText = `Hello, ${userName}! Welcome to my website.`;
-    }
-}
